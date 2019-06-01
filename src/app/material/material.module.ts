2ad8b86985073e7087c03797f50ce146c1361a31
Jan Veldhuis <jan@appsolutions.nl>
2019-06-01 14:17:24 +0200

    Update to Angular Material 8

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 
-import {
-  MatButtonModule,
-  MatMenuModule,
-  MatToolbarModule,
-  MatIconModule,
-  MatCardModule,
-  MatDividerModule,
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
